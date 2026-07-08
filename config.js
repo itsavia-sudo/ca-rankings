e7e91867ef2078e8d0b8667d55817ab238a97e45
itsavia-sudo <itsavia@gmail.com>
2026-07-08 09:06:08 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Temporary placeholder.
 // Before publishing, replace these two values with your Supabase project values.
 window.CA_CONFIG = {
-  SUPABASE_URL: "PASTE_SUPABASE_URL_HERE",
-  SUPABASE_ANON_KEY: "PASTE_SUPABASE_ANON_KEY_HERE"
+  SUPABASE_URL: "https://puiybuxbphzrvlhdrprj.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_ypkcE3mHZN9eZtriyt-rTA_pRfRCfVH"
 };
